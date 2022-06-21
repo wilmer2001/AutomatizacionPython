ed5cf2dc9bd836ddd335071808ff8c711a01b9ad
wilmer2001 <wilmeryesidrodriguez@gmail.com>
2022-06-20 19:50:32 -0500

    Modificaciones se agrega mas Flujos

---
+++ Automatic_navigation.py
@@ -0,0 +1,38 @@
+from lib2to3.pgen2 import driver
+import unittest
+from selenium import webdriver
+#Libreria para poner tiempos
+from time import sleep
+
+class AutomaticNavigation(unittest.TestCase):
+
+	def setUp(self):
+		self.driver = webdriver.Chrome(executable_path= './chromedriver_win32/chromedriver.exe')
+		driver = self.driver
+		driver.implicitly_wait(30)
+		driver.maximize_window()
+		driver.get('https://google.com/')
+
+	def test_browser_navigation(self):
+		driver = self.driver
+
+		search_field = driver.find_element_by_name('q')
+		search_field.clear()
+		search_field.send_keys('platzi')
+		search_field.submit()
+
+
+		driver.back()
+		sleep(3)
+		driver.forward()
+		sleep(3)
+		driver.refresh()
+		sleep(3)
+
+
+
+	def tearDown(self):
+		self.driver.close()
+
+if __name__ == "__main__":
+	unittest.main(verbosity = 2)
